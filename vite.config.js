ac800241ba345eb42d33f5e75f0f67f757df54e9
Zachintosh <zlw11063@gmail.com>
2021-10-26 15:08:26 -0600

    Correct base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/ipsum-generator-demo/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  plugins: [react()],
 })
